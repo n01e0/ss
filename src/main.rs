72ae5e0f1999ef1a5dae29702117c37a77ce9dbf
n01e0 <noleo@vanillabeans.mobi>
2020-07-27 04:05:42 +0900

    refactor

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,16 @@ fn main() {
         ).get_matches();
 
     if let Some(s) = matches.value_of("content") {
-        let mut s = s.chars().collect::<Vec<_>>();
-        s.sort();
-        println!("{}", s.iter().collect::<String>().trim());
+        println!("{}", sort(s.into()));
     } else if matches.is_present("input") {
-        let mut buf = String::new();
-        std::io::stdin().read_line(&mut buf).expect("Can't readline");
-        let mut s = buf.chars().collect::<Vec<_>>();
-        s.sort();
-        println!("{}", s.iter().collect::<String>().trim())
+        let mut s = String::new();
+        std::io::stdin().read_line(&mut s).expect("Can't readline");
+        println!("{}", sort(s))
     }
+}
+
+fn sort(s: String) -> String {
+    let mut s = s.chars().collect::<Vec<_>>();
+    s.sort();
+    s.iter().collect::<String>().trim().into()
 }
